50f56e75468025036b16a489e7995c5f9af0ded6
kirillboychuk <kiryllboychuk@gmail.com>
2024-10-31 16:39:44 +0200

    Add project files.

---
+++ Lesson_9/Lesson_9.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace Kirill
+{
+    class MyClass
+    {
+    public:
+        void showInfo()
+        {
+            cout << "Namespace Kirill: MyClass" << endl;
+
+        }
+    };
+}
+namespace Nazar
+{
+    class MyClass
+    {
+    public:
+        void printInfo()
+        {
+            cout << "Namespace Nazar: MyClass" << endl;
+        }
+    };
+}
+
+int main()
+{
+    //MyClass a; ERROR !!!
+    Kirill::MyClass a;
+    Nazar::MyClass b;
+    a.showInfo();
+    b.printInfo();
+    using namespace Kirill;
+    MyClass c;
+    c.showInfo();
+    using namespace Nazar;
+    Nazar::MyClass d;
+    d.printInfo();
+}
